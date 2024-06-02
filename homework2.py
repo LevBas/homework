41df67efe87a4ec7d07fa16c0013ae0c118feaaf
LevBas <basalaevlev@yandex.ru>
2024-06-02 10:32:48 +0300

    Add files via upload

---
+++ homework2.py
@@ -0,0 +1,5 @@
+completed_works = 12
+hours_spent = 1.5
+course_name = 'Python'
+time_for_one_task = hours_spent/completed_works
+print(course_name, ', всего задач:', completed_works, ', затрачено часов:', hours_spent, ', среднее время выполнения зачачи', time_for_one_task)
